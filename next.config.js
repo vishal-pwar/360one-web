b4332932dcd75dda692da542158fbd3b6635ac5e
Gaurav Jawla <gaurav@pantelwar.com>
2023-08-27 12:49:42 +0530

    fix(next-image): next.config.js configured

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const cmsUrl = new URL(process.env.NEXT_PUBLIC_STRAPI_API_URL);
 const nextConfig = {
+  // images: {
+  //   domains: ["localhost"],
+  // },
   images: {
-    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: isProd ? "https" : "http",
+        hostname: isProd ? cmsUrl.hostname : "localhost",
+        port: isProd ? "" : "1337",
+        pathname: "**",
+      },
+    ],
   },
 };
 
