3e1bd84a161fabb63cc2c007964c30fa2e12e6e0
vishal agarwal <vishal.agarwal@pantelwar.com>
2023-11-07 13:03:14 +0530

    fix(mixpanel): fixed mixpanel issues

--- src/components/mixpanel/index.tsx
+++ src/components/mixpanel/index.tsx
@@ -3,23 +3,38 @@ import mixpanel from "mixpanel-browser";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN || "", {
-  debug: true,
-  track_pageview: true,
-  ignore_dnt: true,
-});
+try {
+  if (!process.env.NEXT_PUBLIC_MIXPANEL_TOKEN) {
+    throw new Error("mix panel token missing from .env");
+  }
+
+  mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN, {
+    debug: true,
+    track_pageview: true,
+    ignore_dnt: true,
+  });
+} catch (error) {
+  console.log(error);
+}
 
 const Mixpanel = () => {
   const pathName = usePathname();
 
   React.useEffect(() => {
-    if (pathName)
+    if (
+      pathName &&
+      "mixpanel" in window &&
+      typeof window.mixpanel === "object" &&
+      window.mixpanel !== null &&
+      "track" in window.mixpanel &&
+      typeof window.mixpanel.track === "function"
+    )
       mixpanel.track("Page View", {
         url: pathName,
       });
   }, [pathName]);
 
-  return <div></div>;
+  return null;
 };
 
 export default Mixpanel;
