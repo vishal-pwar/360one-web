fa84fc6e61fdeb60c2254a6220bef03ffcaef024
Gaurav Jawla <gaurav@pantelwar.com>
2023-11-28 12:53:18 +0530

    fix(vapt): update next.config.js with appropriate security headers

--- next.config.js
+++ next.config.js
@@ -15,6 +15,37 @@ const nextConfig = {
     ],
   },
   output: "standalone",
+  // Adding policies:
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(); battery=(); geolocation=(); microphone=()",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
